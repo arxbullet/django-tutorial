eed84f04f0d55b10bf3dedf49d4cb06f64f1922e
arx_bullet <aranzav.c.b@gmail.com>
2022-02-08 18:03:26 -0300

    init proyect

--- miapp/manage.py
+++ miapp/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tutorialDjango.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'miapp.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
