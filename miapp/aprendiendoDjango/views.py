f9882c6f33aca91c361bce2796627531318d5ce9
arx_bullet <aranzav.c.b@gmail.com>
2022-02-08 18:31:04 -0300

    primera vista con django

--- miapp/aprendiendoDjango/views.py
+++ miapp/aprendiendoDjango/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+#mvc = modelo vista controlador
+#mvt = modelo vista templates
+
+def hola_mundo(request):
+    #return HttpResponse('holamundo con django') esto es una respuesta http
+    return HttpResponse("""<h1>holamundo con django</h1>""") #tambien puedo devolver html
+
+
+#para que este metodo funcione debo cargarlo en una url en urls.py
+
