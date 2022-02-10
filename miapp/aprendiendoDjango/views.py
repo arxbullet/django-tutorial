1729d93d6a198e4d7241b007667cb3e359fe725a
arx_bullet <aranzav.c.b@gmail.com>
2022-02-10 19:22:01 -0300

    vistas en django

--- miapp/aprendiendoDjango/views.py
+++ miapp/aprendiendoDjango/views.py
@@ -5,6 +5,17 @@ from django.shortcuts import render, HttpResponse
 #mvc = modelo vista controlador
 #mvt = modelo vista templates
 
+
+def index(request):#inicio
+    return HttpResponse("""
+    <h1>inicio</h1>
+    """)
+
+def pagina(request):
+    return HttpResponse("""
+    <h1>pagina de mi web</h1>
+    """)
+
 def hola_mundo(request):
     #return HttpResponse('holamundo con django') esto es una respuesta http
     return HttpResponse("""<h1>holamundo con django</h1>""") #tambien puedo devolver html
