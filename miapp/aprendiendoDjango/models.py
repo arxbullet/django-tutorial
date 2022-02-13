377013623cca6e5508427e8c3a5e361f2da13d24
arx_bullet <aranzav.c.b@gmail.com>
2022-02-13 18:42:16 -0300

    crear modelos

--- miapp/aprendiendoDjango/models.py
+++ miapp/aprendiendoDjango/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+# pequeñas clases que nos generan objetos para trabajar con las entidades bd
+# cada modelo representa una entidad y define los campos de un objeto
+
+#crear modelo
+
+class Article(models.Model): #esto creara la tabla article
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    public = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+class Category(models.Model): 
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
