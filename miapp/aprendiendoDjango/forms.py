2e4d68801dcacb9f05850eb16d79da92d64b58be
arx_bullet <aranzav.c.b@gmail.com>
2022-02-14 20:18:35 -0300

    widget y atributos forms django

--- miapp/aprendiendoDjango/forms.py
+++ miapp/aprendiendoDjango/forms.py
@@ -1,9 +1,20 @@
+from turtle import textinput
 from django import forms
 
 class FormularioArticulo(forms.Form):
     title = forms.CharField(
-        label='Titulo'
+        label='Titulo',
+        # otros atributos
+        required=True,
+        max_length=50,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder' : 'pon el titulo',
+                'class': 'titulo_form_article'
+            }
+        )
     )
+
     content = forms.CharField(
          label='Contenido',
          widget=forms.Textarea
